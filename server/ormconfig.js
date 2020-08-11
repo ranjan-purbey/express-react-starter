33c9da243b8e66d3807e2ae66e3f3b5db34aa955
Ranjan Purbey <ranjan.purbey@gmail.com>
2020-08-12 05:22:04 +0530

    add db migration in start script

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,10 +1,16 @@
 module.exports = {
   type: "postgres",
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  database: process.env.POSTGRES_DB,
-  username: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
+  ...(process.env.DATABASE_URL
+    ? {
+        url: process.env.DATABASE_URL,
+      }
+    : {
+        host: process.env.POSTGRES_HOST,
+        port: process.env.POSTGRES_PORT,
+        database: process.env.POSTGRES_DB,
+        username: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+      }),
   uuidExtension: "pgcrypto",
   synchronize: false,
   logging: true,
